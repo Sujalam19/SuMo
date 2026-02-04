76da9a7de94bc96bf149856750f6bd689258388a
Sujalam19 <sujalampandeym@gmail.com>
2026-02-04 16:54:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// For surprise page
+document.getElementById('surprise-btn').addEventListener('click', function() {
+    document.getElementById('gift').style.display = 'block';
+    this.style.display = 'none';
+});
